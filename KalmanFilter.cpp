3ad152a80e631077611715704bb1d60c500d4a3c
silvaordie <40876673+silvaordie@users.noreply.github.com>
2019-07-09 17:30:37 +0100

    Kalman filter

---
+++ KalmanFilter.cpp
@@ -0,0 +1,80 @@
+#include <Kalman.h>
+#include <ctime>    
+#include<Wire.h>
+
+class KalmanFilter{
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+char tmp_str[7];
+};
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  char* tmp_str=null;
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+float update(int16_t accX , int16_t accY , int16_t accZ,int16_t gyroX,int16_t gyroY,int16_t gyroZ, float dt){
+  //double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+
+  
+  #ifdef RESTRICT_PITCH // Eq. 25 and 26
+      double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+      double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+    #else // Eq. 28 and 29
+      double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+      double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+    #endif
+    
+    double gyroXrate = gyroX / 131.0; // Convert to deg/s
+    double gyroYrate = gyroY / 131.0; // Convert to deg/s
+  
+    #ifdef RESTRICT_PITCH
+      // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+      if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+        kalmanX.setAngle(roll);
+        compAngleX = roll;
+        kalAngleX = roll;
+        gyroXangle = roll;
+      } else
+        kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+    
+      if (abs(kalAngleX) > 90)
+        gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+      kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+    #else
+      // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+      if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+        kalmanY.setAngle(pitch);
+        compAngleY = pitch;
+        kalAngleY = pitch;
+        gyroYangle = pitch;
+      } else
+        kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+    
+      if (abs(kalAngleY) > 90)
+        gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+      kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+    #endif
+    
+      gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+      gyroYangle += gyroYrate * dt;
+      //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+      //gyroYangle += kalmanY.getRate() * dt;
+    
+      compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+      compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+    
+      // Reset the gyro angle when it has drifted too much
+      if (gyroXangle < -180 || gyroXangle > 180)
+        gyroXangle = kalAngleX;
+      if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+    
+    return kalAngleX
+}
---
+++ KalmanFilter.h
@@ -0,0 +1,2 @@
+float update(int16_t accX,int16_t accY,int16_t accZ,int16_t gyroX,int16_t gyroY,int16_t gyroZ,float dt);
+char* convert_int16_to_str(int16_t i);
